e5f08b1afea25549f0717a3bc284183df257589b
Jacob <govern01mail@gmail.com>
2021-08-01 16:26:23 +1000

    Slash Command implemented

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 from configparser import ConfigParser
 from cogs.admin import AdminCog
 from cogs.welcome import WelcomeCog
 
 cfg = ConfigParser()
 cfg.read("bot.ini")
-bot = commands.Bot(command_prefix=cfg['BOT']['command_prefix'])
+bot = commands.Bot(command_prefix=cfg['BOT']['command_prefix'], intents=discord.Intents.all())
 
-@bot.command(name="ping")
-async def server_response(ctx):
-    await ctx.reply("pong")
+GUILD_IDS = [204104761241370624]
+
+slash = SlashCommand(bot, sync_commands=True)
+
+@slash.slash(name="ping")
+async def server_response(ctx: SlashContext):
+    await ctx.send("pong")
 
 @bot.event
 async def on_ready():
     print("Ready!!")
+
+
 
 
 bot.add_cog(AdminCog(bot))
