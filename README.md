b54d7d0c64f2e379bc561f69c13487b5ef61bad4
Jacob <govern01mail@gmail.com>
2021-08-01 18:59:22 +1000

    Implemented voice chat - Can join - Can leave - Can play sound effects

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Discord Tutorial Bot
 
-This project is a tutorial for creating bots with discord.py; It will handle creating commands, COGs, slash commands and voice chat.
+This project is a tutorial for creating bots with disco<span>rd.</span>py; It will handle creating commands, COGs, slash commands and voice chat.
+
+Sound effects obtained from <https://www.zapsplat.com>
--- main.py
+++ main.py
@@ -21,8 +21,28 @@ async def server_response(ctx: SlashContext):
 async def on_ready():
     print("Ready!!")
 
+@bot.command(name="join")
+async def join_voice_chat(ctx):
+    voice_channel = ctx.author.voice.channel
+    await voice_channel.connect()
 
+@bot.command(name="leave")
+async def leave_voice_chat(ctx):
+    voice_clients = bot.voice_clients
+    for client in voice_clients:
+        if client.guild.id == ctx.guild.id:
+            await client.disconnect()
+            break
 
+@bot.command(name="play")
+async def play_sound_effect(ctx, sound_effect_title: str):
+    sound_effect_path = f"./sound_effects/{sound_effect_title}.mp3"
+    sound_effect = discord.FFmpegPCMAudio(sound_effect_path)
+    voice_clients = bot.voice_clients
+    for client in voice_clients:
+        if client.guild.id == ctx.guild.id:
+            client.play(sound_effect)
+            break
 
 bot.add_cog(AdminCog(bot))
 bot.add_cog(WelcomeCog(bot))
